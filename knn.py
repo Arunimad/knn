fc79271a240dea51ab70a07c5bcca1c5eac5bddc
root <root@localhost.localdomain>
2017-10-15 02:30:17 +0000

    Test Commit

---
+++ knn.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+input = input("1)Test 2)KNN")
+
+def test():
+
+      print" KNN on a test data"
+
+def KNN():
+     print "KNN Algorithm"
+
+if input == 1:
+   test()
+else:
+   knn()
