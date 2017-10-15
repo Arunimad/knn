5bb99c72548fbb10366e4c381a7be5849157c260
root <root@localhost.localdomain>
2017-10-15 20:45:02 +0000

    Data-setted

--- knn.py
+++ knn.py
@@ -1,20 +1,32 @@
+import csv
+import random
+import pandas as pd
+import numpy as np
+
+
+#with open('sample.csv','rb') as csvfile:
+ #   lines = csv.reader(csvfile)
+  #  for row in lines:
+   #   print row
 
 
 
+df = pd.read_csv('sample.csv')
+colnames=['Day','Gender']
+df = pd.read_csv('sample.csv', names=colnames, header=None)
+
+df['Day']= np.random.randint(1,31, df.shape[0])
+df['Gender']= np.random.randint(0,2, df.shape[0])
 
 
 
+input = input("Enter no. btw 1-100")
 
-input = input("1)Test 2)KNN")
 
-def test():
 
-      print" KNN on a test data"
+day=df['Day'].values
+gender=df['Gender'].values
 
-def KNN():
-     print "KNN Algorithm"
-
-if input == 1:
-   test()
-else:
-   knn()
+print df 
+print day
+print gender
